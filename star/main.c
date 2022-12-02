bdb7cc19ea7c47d7ad5b44045bb849cd8efdb3d5
amirMahfoozi <amirmhfz@yahoo.com>
2022-12-02 22:59:42 +0330

    deleted the last part

--- star/main.c
+++ star/main.c
@@ -75,25 +75,5 @@ int main()
         }
         printf("\n");
     }
-    big = ((n-1)/4)*paen + ((n-1)/4)*bala;
-    for(int i = (n-1)/4-1;i>=0;i--)
-    {
-        for(int j = 0;j<big-i*bala;j++)
-        {
-            printf("-");
-        }
-        printf("*");
-        for(int z = 0;z<2*bala*i-1;z++)
-        {
-            printf("-");
-        }
-        if(i!=0 && bala != 0)
-            printf("*");
-        for(int j = 0;j<big-i*bala;j++)
-        {
-            printf("-");
-        }
-        printf("\n");
-    }
     return 0;
 }
