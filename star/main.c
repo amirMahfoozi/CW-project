e176261efa8be1ab6141233dd9dc0deb7a6f2857
amirMahfoozi <amirmhfz@yahoo.com>
2022-12-02 23:08:43 +0330

    reverted

--- star/main.c
+++ star/main.c
@@ -50,50 +50,5 @@ int main()
         }
         printf("\n");
     }
-    for(int i = (n-1)/4-1;i>=0;i--)
-    {
-        for(int j = 0;j<big-i*paen;j++)
-        {
-            printf("-");
-        }
-        printf("*");
-        for(int z = 0;z<2*bala*(n-1)/4-1+2*i*paen;z++)
-        {
-            printf("-");
-        }
-        if(bala == 0 && i == 0)
-        {
-
-        }
-        else
-        {
-            printf("*");
-        }
-        for(int j = 0;j<big-i*paen;j++)
-        {
-            printf("-");
-        }
-        printf("\n");
-    }
-    big = ((n-1)/4)*paen + ((n-1)/4)*bala;
-    for(int i = (n-1)/4-1;i>=0;i--)
-    {
-        for(int j = 0;j<big-i*bala;j++)
-        {
-            printf("-");
-        }
-        printf("*");
-        for(int z = 0;z<2*bala*i-1;z++)
-        {
-            printf("-");
-        }
-        if(i!=0 && bala != 0)
-            printf("*");
-        for(int j = 0;j<big-i*bala;j++)
-        {
-            printf("-");
-        }
-        printf("\n");
-    }
     return 0;
 }
