98a94719fe76f5b9d02c54dc64733ec423afeb77
amirMahfoozi <amirmhfz@yahoo.com>
2022-12-02 22:20:19 +0330

    updated the second-quarter of the star

--- star/main.c
+++ star/main.c
@@ -23,5 +23,33 @@ int main()
         }
         printf("\n");
     }
+    big = ((n-1)/4)*paen;
+    for(int i = 0;i<(n-1)/4+1;i++)
+    {
+        for(int j = 0;j<big-i*paen;j++)
+        {
+            printf("-");
+        }
+        printf("*");
+        for(int z = 0;z<2*bala*(n-1)/4-1+2*i*paen;z++)
+        {
+            printf("-");
+        }
+        if(bala == 0 && i == 0)
+        {
+
+        }
+        else
+        {
+            printf("*");
+        }
+
+        for(int j = 0;j<big-i*paen;j++)
+        {
+            printf("-");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
